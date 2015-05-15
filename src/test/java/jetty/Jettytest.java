5ab06a99f7879d0ac0222f233c75c3d2a4b91a9e
qinq <1247686880@qq.com>
2015-05-15 10:26:32 +0800

    jetty

--- src/test/java/jetty/Jettytest.java
+++ src/test/java/jetty/Jettytest.java
@@ -1,13 +1,38 @@
 package jetty;
 
-import org.junit.Test;
+/*import java.lang.reflect.Field;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.Test;*/
 
 public class Jettytest {
+	
+	public static final int PORT = 9090;
+	 
+	public static void main(String[] args) {
+		
+		
+/*		// TODO Auto-generated method stub
+		String name=Class.class.getClass().getResource("/").getPath();
+		
+		Server server = new Server();
+		// 设置在JVM退出时关闭Jetty的钩子。
+		server.setStopAtShutdown(true);
 
-	@Test
-	public void jetty() {
-		// TODO Auto-generated method stub
-		System.out.print("AAA");
+		SelectChannelConnector connector = new SelectChannelConnector();
+		connector.setPort(port);
+		// 解决Windows下重复启动Jetty居然不报告端口冲突的问题.
+		connector.setReuseAddress(false);
+		server.setConnectors(new Connector[] { connector });
+
+		WebAppContext webContext = new WebAppContext(DEFAULT_WEBAPP_PATH, contextPath);
+		// 修改webdefault.xml，解决Windows下Jetty Lock住静态文件的问题.
+		webContext.setDefaultsDescriptor(WINDOWS_WEBDEFAULT_PATH);
+		server.setHandler(webContext);
+		System.out.print(name);*/
 	}
 
 }
