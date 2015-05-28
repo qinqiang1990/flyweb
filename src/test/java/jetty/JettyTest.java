17dea67fae11ad17354cb8c3c87e506708dfa02d
qinq <1247686880@qq.com>
2015-05-28 18:42:53 +0800

    jetty

--- src/test/java/jetty/JettyTest.java
+++ src/test/java/jetty/JettyTest.java
@@ -1,50 +1,66 @@
 package jetty;
+ 
+import java.util.List;
 
-import org.eclipse.jetty.server.Connector;
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
 import org.eclipse.jetty.webapp.WebAppClassLoader;
 import org.eclipse.jetty.webapp.WebAppContext;
-import org.junit.Test;
 
-/*import java.lang.reflect.Field;
-
- import org.eclipse.jetty.server.Connector;
- import org.eclipse.jetty.server.Server;
- import org.eclipse.jetty.server.nio.SelectChannelConnector;
- import org.eclipse.jetty.webapp.WebAppContext;
- import org.junit.Test;*/
+import com.google.common.collect.Lists;
+ 
 
 public class JettyTest {
 
-	private final int PORT = 9090;
+	private static final int PORT = 8080;
 
-	private final String DEFAULT_WEBAPP_PATH = "src/main/webapp";
+	private static final String DEFAULT_WEBAPP_PATH = "src/main/webapp";
 
-	private final String DEFAULT_CONTEXT_PATH = "/flyweb";
+	private static final String DEFAULT_CONTEXT_PATH = "/flyweb";
 
-	private final String DEFAULT_WEBDEFAULTXML_PATH = "src/main/webapp/WEB-INF/webdefault.xml";
+	private static final String DEFAULT_WEBDEFAULTXML_PATH = "target/flyweb/WEB-INF/webdefault.xml";
 
-	private final String DEFAULT_WEBXML_PATH = "src/main/webapp/WEB-INF/web.xml";
+	private static final String DEFAULT_WEBXML_PATH = "target/flyweb/WEB-INF/web.xml";
 
-	@Test
-	public void start() throws Exception {
+	public static void main(String[] args) throws Exception {
 
 		Server server = new Server(PORT);
-//war
-		WebAppContext context = new WebAppContext();
-		context.setContextPath(DEFAULT_CONTEXT_PATH);
-		context.setResourceBase(DEFAULT_WEBAPP_PATH);
+		// war
+		WebAppContext context = new WebAppContext(DEFAULT_WEBAPP_PATH,
+				DEFAULT_CONTEXT_PATH);
+
 		context.setDefaultsDescriptor(DEFAULT_WEBDEFAULTXML_PATH);
 		context.setDescriptor(DEFAULT_WEBXML_PATH);
 		
+	 
+		List<String> jarNameExprssions = Lists.newArrayList("/flyweb/WEB-INF/lib/*[^/]*\\.jar$");
+		  
+		context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
+				StringUtils.join(jarNameExprssions, '|'));
+
+		
+		/*
 		WebAppClassLoader classLoader = new WebAppClassLoader(context);
 		classLoader.addClassPath("target/classes");
-		context.setClassLoader(classLoader);
-
+		context.setClassLoader(classLoader);*/
+  
+		//context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern","target/flyweb/WEB-INF/lib/.*\\.jar$");
+ 
 		server.setHandler(context);
 
-		server.start();
-		server.join();
+		server.start(); 
+		
+		System.out.println("[INFO] Server running at http://localhost:" + PORT + DEFAULT_CONTEXT_PATH);
+		System.out.println("[HINT] Hit Enter to reload the application quickly");
+
+		// 等待用户输入回车重载应用.
+		while (true) {
+			char c = (char) System.in.read();
+			if (c == '\n') {
+
+				server.stop();
+				server.start();
+			}
+		}
 	}
 }
---
+++ src/test/java/jetty/QuickStartServer.java
@@ -0,0 +1,41 @@
+package jetty;
+
+import org.eclipse.jetty.server.Server;
+ 
+
+/**
+ * 使用Jetty运行调试Web应用, 在Console输入回车快速重新加载应用.
+ * dcw
+ */
+public class QuickStartServer {
+
+	public static final int PORT = 9090;
+	public static final String CONTEXT = "/";
+	public static final String[] TLD_JAR_NAMES = new String[] { "sitemesh", "spring-webmvc", "shiro-web",
+			"springside-core" };
+
+	public static void main(String[] args) throws Exception {
+	 
+		// 启动Jetty
+		Server server = JettyFactory.createServerInSource(PORT, CONTEXT);
+		JettyFactory.setTldJarNames(server, TLD_JAR_NAMES);
+
+		try {
+			server.start();
+
+			System.out.println("[INFO] Server running at http://localhost:" + PORT + CONTEXT);
+			System.out.println("[HINT] Hit Enter to reload the application quickly");
+
+			// 等待用户输入回车重载应用.
+			while (true) {
+				char c = (char) System.in.read();
+				if (c == '\n') {
+					JettyFactory.reloadContext(server);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
+}
