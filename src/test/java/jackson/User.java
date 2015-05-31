daf5ff4427c82acf47593cf5b2159b8706b30759
qinqiang1990 <1247686880@qq.com>
2015-06-01 03:08:45 +0800

    jackjson

---
+++ src/test/java/jackson/User.java
@@ -0,0 +1,72 @@
+package jackson;
+
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+	private String name;
+
+	// 不JSON序列化年龄属性
+	@JsonIgnore
+	private Integer age;
+
+	// 格式化日期属性
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", locale = "zh", timezone = "GMT+8")
+	private Date birthday;
+
+	// 序列化email属性为mail
+	@JsonProperty("mail")
+	private String email;
+
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date date;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
